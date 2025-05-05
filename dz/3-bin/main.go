0f107f09332f1957b51b8c92e3143db29f95e9c1
Филичев Валерий <colisey@inbox.ru>
2025-05-05 12:09:44 +0300

    14.9. Домашнее задание - Env и шифрование

--- dz/3-bin/main.go
+++ dz/3-bin/main.go
@@ -4,9 +4,15 @@ import (
 	"main/storage"
 
 	"github.com/fatih/color"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		color.Red("Error loading .env file")
+	}
+
 	binlist, _ := storage.Read("data.json")
 	if binlist != nil {
 		for _, bin := range binlist.Bins {
